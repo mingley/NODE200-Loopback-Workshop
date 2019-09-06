e41fac04edbb26ec1835b0482a4955456482f559
mikeingley <michael.ingley@gmail.com>
2019-09-06 15:13:44 -0700

    1

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -6,7 +6,7 @@ module.exports = {
     "MongoDB": {
       "host": "",
       "port": 0,
-      "url": "",
+      "url": "mongodb://localhost:27017",
       "database": "",
       "password": "",
       "name": "MongoDB",
